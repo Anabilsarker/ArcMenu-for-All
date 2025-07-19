4e2879a3e262188edc27df5efc4e5928c94b6ca1
Anabilsarker <sarkeranabil@gmail.com>
2025-07-20 01:15:49 +0600

    Add Wayland class for special hovering window in Linux
    Introduces a new `Wayland` class in the `ArcMenu_for_All.Platform.Linux` namespace. This class includes a constant for the Wayland client library and a P/Invoke method to connect to a Wayland display.

---
+++ Platform/Linux/Wayland.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ArcMenu_for_All.Platform.Linux
+{
+    internal class Wayland
+    {
+        private const string LibWaylandClient = "libwayland-client.so.0"; // Not how it is done.
+
+        // Core display functions
+        [DllImport(LibWaylandClient)]
+        public static extern IntPtr wl_display_connect(string name);
+    }
+}
